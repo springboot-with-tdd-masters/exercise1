34be57872bfe245c3fd479869c8a424f5c4284ac
Christian Perez <christianpe@magenic.com>
2022-05-04 20:35:49 +0800

    Added @Repository annotation

--- src/main/java/com/example/exercise1/repository/BookRepository.java
+++ src/main/java/com/example/exercise1/repository/BookRepository.java
@@ -1,9 +1,11 @@
 package com.example.exercise1.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
 import com.example.exercise1.model.Book;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long>{
 
 }
