1e9a98b7213c1eb524b28382bd12e66594410506
marionino.obog <marioo@magenic.com>
2022-05-04 18:23:43 +0800

    initial commit

---
+++ src/test/java/com/magenic/mobog/exercise1app/Exercise1AppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.magenic.mobog.exercise1app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Exercise1AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
